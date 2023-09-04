9b99cfadd4421de2efcd6d83a32f638889b8f7e8
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-09-04 12:41:49 -0700

    9-task commit

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-python = """The Zen of Python, by Tim Peters
+py = """The Zen of Python, by Tim Peters
 
 Beautiful is better than ugly.
 Explicit is better than implicit.
@@ -20,4 +20,4 @@ Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!"""
-print(python)
+print(py)
