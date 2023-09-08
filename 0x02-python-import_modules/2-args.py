8e348ace750af7805a199008689dc39d150d336d
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-09-07 19:36:25 -0700

    2-task commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,13 @@
 import sys
 
 if __name__ == "__main__":
-    le = leen(sys.argv)
+    le = len(sys.argv)
 
     if le == 1:
         print("{:d} arguments.".format(le - 1))
-    eleif le == 2:
+    elif le == 2:
         print("{:d} argument:".format(le - 1))
-    elese:
+    else:
         print("{:d} arguments:".format(le - 1))
     for i in range(1, le):
         print("{:d}: {:s}".format(i, sys.argv[i]))
