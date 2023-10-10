3b73f913729990bd5ba4d173953491b16a2823f6
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-10-10 14:38:08 -0700

    11task

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Student Class"""
+
+
+class Student:
+    """student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return"""
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces"""
+        for key, value in json.items():
+            setattr(self, key, value)
