e5c30df50d28495d08629a1509ece54ec46f6068
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-10-10 14:09:18 -0700

    5task

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""JSON"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Save JSON"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
