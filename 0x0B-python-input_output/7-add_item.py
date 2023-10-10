00b689b8e5ed03e28063b0422aeead935c2b554d
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-10-10 14:19:40 -0700

    7task

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Load,add,save"""
+
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+f = "add_item.json"
+
+try:
+    list = load_from_json_file(f)
+except FileNotFoundError:
+    list = []
+
+for arg in argv[1:]:
+    list.append(arg)
+
+save_to_json_file(list, f)
