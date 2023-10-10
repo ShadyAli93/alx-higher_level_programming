233ae79feee8be4af886ead4e4c21ef71ac7e556
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-10-10 14:33:55 -0700

    10task

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -10,6 +10,9 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """returns"""
+    def to_json(self, attrs=None):
+        """return"""
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
