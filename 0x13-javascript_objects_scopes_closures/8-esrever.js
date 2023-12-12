d81a0edabb7be865f8c88065959fb619fc5da730
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-12-12 12:45:44 -0800

    8task

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let reversed = [];
-  for (let index = list.length - 1; index >= 0; index--) {
-    reversed.push(list[index]);
-  }
-  return reversed;
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
